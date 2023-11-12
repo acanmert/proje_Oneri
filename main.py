d244a80e52998a873bb9b42b7d56188325d12fbd
acanmert <ahmetcanmert25@gmail.com>
2023-11-12 17:25:37 +0300

    son hali

--- main.py
+++ main.py
@@ -3,50 +3,90 @@ import matplotlib.pyplot as plt
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-movie_data =pd.read_csv('data.csv')
+# CSV dosyasından film verilerini yükle
+secim=int(input("Hangi veri setini tercih edersiniz:\n 1-)Film \n 2-)Kitap \n 3-)Kıyafet"))
+if secim==1:
+    data = pd.read_csv('data.csv', encoding='ISO-8859-9')
+if secim ==2:
+    data = pd.read_csv('book_data.csv', encoding='ISO-8859-9')
+if secim ==3:
+    data = pd.read_csv('clouth_data1.csv', sep=';', encoding='ISO-8859-9')
 
-movie_data['combined_features'] = movie_data.apply(lambda row: ' '.join([str(row['title']), str(row['genre']), str(row['director']), str(row['actors'])]), axis=1)
 
+
+print(data.columns)
+# Kullanıcının seçtiği özelliklere göre birleştirilmiş özellikleri oluşturan fonksiyon
+def create_combined_features(row, selected_features):
+    return ' '.join([str(row[feature]) for feature in selected_features])
+
+# Benzerlik hesaplamada kullanılacak özellikleri kullanıcıdan al
+selected_features = input("Benzerlik hesaplamak için özellikleri girin (örneğin, title, genre, actors, director, overview): ").split(',')
+
+# Kullanıcının seçtiği özelliklere göre 'combined_features' sütununu oluştur
+data['combined_features'] = data.apply(lambda row: create_combined_features(row, selected_features), axis=1)
+
+# Birleştirilmiş özellikleri vektörleştir
 vectorizer = TfidfVectorizer()
-tfidf_matrix = vectorizer.fit_transform(movie_data['combined_features'])
+tfidf_matrix = vectorizer.fit_transform(data['combined_features'])
 
+# Kosinüs benzerliği hesapla
 cosine_sim = cosine_similarity(tfidf_matrix, tfidf_matrix)
 
-def get_movie_recommendations(movie_title, top_n=10):
-    # Find the index of the movie in the dataset
-    movie_index = movie_data[movie_data['title'] == movie_title].index[0]
+# Kullanıcının girdiği film başlığına göre film önerilerini almak için fonksiyon
+def get_movie_recommendations(title, top_n=10,):
+    if secim==1:
+        index = data[data['title'] == title].index[0]
+        similarity_scores = list(enumerate(cosine_sim[index]))
+        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+        top_recommendations = similarity_scores[1:top_n + 1]
+        data1='title'
+        data2='genre'
+        data3='director'
+    elif secim==2:
+        index = data[data['isim'] == title].index[0]
+        similarity_scores = list(enumerate(cosine_sim[index]))
+        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+        top_recommendations = similarity_scores[1:top_n + 1]
+        data1='isim'
+        data2='tur'
+        data3='yazar'
+    elif  secim==3:
+        index = data[data['numara'] == int(title)].index[0]
+        similarity_scores = list(enumerate(cosine_sim[index]))
+        similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
+        top_recommendations = similarity_scores[1:top_n + 1]
+        data1='isim'
+        data2='firma'
+        data3='numara'
 
-    # Get the similarity scores for the movie
-    similarity_scores = list(enumerate(cosine_sim[movie_index]))
 
-    # Sort movies based on similarity scores
-    similarity_scores = sorted(similarity_scores, key=lambda x: x[1], reverse=True)
 
-    # Get top recommendations
-    top_recommendations = similarity_scores[1:top_n + 1]
+    recommended =[[data[data1][top_data[0]],data[data2][top_data[0]],data[data3][top_data[0]]] for top_data in top_recommendations]
+    return recommended, [score[1] for score in top_recommendations]
 
-    # Retrieve movie titles for the recommended movie indices
-    recommended_movies = [movie_data['title'][movie[0]] for movie in top_recommendations]
+# Kullanıcıdan film başlığı girdisi al
+title = input("Aradığınızı Girin: ")
 
-    return recommended_movies, [score[1] for score in top_recommendations]
+# Kullanıcının girdisine göre film önerilerini al
+recommendations, similarity_scores = get_movie_recommendations(title, top_n=10)
 
-movie_title = input("Enter a movie title: ")
-
-recommendations, similarity_scores = get_movie_recommendations(movie_title, top_n=10)
-
+# Sonuçları çiz ve göster
 plt.figure(figsize=(10, 6))
-plt.barh(recommendations, similarity_scores)
-plt.xlabel('Cosine Similarity')
-plt.ylabel('Movie Title')
-plt.title(f"Top 10 Recommended Movies for '{movie_title}' based on Cosine Similarity")
+plt.barh(range(len(recommendations)), similarity_scores)
+
+plt.xlabel('Kosinüs Benzerliği')
+plt.ylabel('Film Başlığı')
+plt.title(f"'{title}' için Kosinüs Benzerliğine Göre En İyi 10 Tavsiye Film")
 plt.tight_layout()
 plt.show()
 
+# Tavsiyeleri bir metin dosyasına yaz
 output_file = 'output.txt'
 with open(output_file, 'w', encoding='utf-8') as file:
-    file.write(f"Top 10 Recommended Movies for '{movie_title}':\n")
-    for movie in recommendations:
-        file.write(movie + '\n')
+    file.write(f"'{title}' için En İyi 10 Tavsiye Film:\n")
+    i=0
+    for sonuc in recommendations:
 
-
-print(f"Recommended movies for '{movie_title}' have been written to '{output_file}'.")
+        file.write(str(sonuc[0]) + ',' + str(sonuc[1]) + ',' + str(sonuc[2]) + ',' + str(similarity_scores[i]) + ',' + '\n')
+        i=i + 1
+print(f"'{title}' için önerilen filmler '{output_file}' dosyasına yazıldı.")
